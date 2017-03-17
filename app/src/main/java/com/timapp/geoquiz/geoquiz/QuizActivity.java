b0bc9c0c14dd4126f13397efc6f9fd8be87ad4ea
timqae <timqae@gmail.com>
2017-03-17 18:57:04 +0200

    2d

--- app/src/main/java/com/timapp/geoquiz/geoquiz/QuizActivity.java
+++ app/src/main/java/com/timapp/geoquiz/geoquiz/QuizActivity.java
@@ -2,8 +2,12 @@ package com.timapp.geoquiz.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
